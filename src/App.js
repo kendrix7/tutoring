9280e8d3b03c9359d04b9365ee18de23820ad14b
Mark Nielsen <marknielsen7@outlook.com>
2018-03-13 19:56:48 -0600

    Added links with opening CSS

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { HashRouter } from 'react-router-dom';
 import router from './router.js';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import './Home.css';
 
 export default class Home extends Component {
     render() {
         return (
-            <div>
-                Hi!
+            <div className='home' >
+                <div className='home_top'>
+                    <div className='home_top_left' >
+                        <Link className='link' data-link='ACT' to='/ACT' >ACT</Link >
+                        <Link className='link' id='SAT_link' data-link='SAT' to='SAT' >SAT</Link >
+                    </div>
+
+                    <div className='home_top_right'>
+                        <div className='' ></div>
+                    </div>
+                </div>
+
+                <div>
+
+                </div>
+
             </div>
         )
     }
